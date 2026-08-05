bd82110f7da163d30d1164e84b092e264dacd15e
lucassoalheiro <lucas.soalheiro.p@gmail.com>
2026-08-05 20:31:37 -0300

    feat: add string, number, boolean and null parsing

--- src/parser.c
+++ src/parser.c
@@ -1,6 +1,21 @@
 #include "parser.h"
-#include <stdio.h>
-#include <string.h>
+
+bool is_number(char number) {
+  char numbers[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '.'};
+  for (size_t i = 0; i < 12; i++) {
+    if (number == numbers[i])
+      return true;
+  }
+  return false;
+}
+
+char *null_token(char *json, size_t *index) {
+  if (strncmp(json + *index, "null", 4) == 0) {
+    *index += 3;
+    return strdup("null");
+  }
+  return NULL;
+}
 
 char *string_token(char *json, size_t *index) {
   size_t end = *index + 1;
@@ -10,41 +25,54 @@ char *string_token(char *json, size_t *index) {
   (*index)++;
   size_t string_size = (end) - *index;
   char *string = (char *)malloc(string_size + 1);
+  if (string == NULL) {
+    return NULL;
+  }
   for (size_t i = 0; i < string_size; i++) {
     string[i] = json[*index];
     (*index)++;
   }
+  string[string_size] = '\0';
   return string;
 }
 char *boolean_token(char *json, size_t *index) {
-  size_t end = *index;
-  while (json[end] != ',' && json[end] != '}') {
-    end++;
+  if (strncmp(json + *index, "true", 4) == 0) {
+    *index += 3;
+    return strdup("true");
   }
-  size_t boolean_size = end - *index;
-  char *boolean = (char *)malloc(boolean_size + 1);
-  for (size_t i = 0; i < boolean_size; i++) {
-    boolean[i] = json[*index];
-    (*index)++;
+  if (strncmp(json + *index, "false", 5) == 0) {
+    *index += 4;
+    return strdup("false");
   }
-  return boolean;
+  return NULL;
 }
 char *number_token(char *json, size_t *index) {
   size_t end = *index;
-  while (json[end] != ',' && json[end] != '}') {
+  while (json[end] != ',' && json[end] != '}' && json[end] != ']') {
     end++;
   }
   size_t number_size = end - *index;
+
   char *number = (char *)malloc(number_size + 1);
+  if (number == NULL) {
+    return NULL;
+  }
+
   for (size_t i = 0; i < number_size; i++) {
     number[i] = json[*index];
+    if (!is_number(json[*index])) {
+      free(number);
+      return NULL;
+    }
     (*index)++;
   }
+  number[number_size] = '\0';
   return number;
 }
 
 void lexer() {
-  char json[] = "{\"name\":\"Lucas\",\"age\":19,\"isStudent\":true}\0";
+  char json[] =
+      "{\"name\":\"Lucas\",\"age\":-19.90,\"isStudent\":true,\"money\":null}";
   JSON *json_lexer = (JSON *)malloc(strlen(json) * sizeof(JSON));
   char *token;
   for (size_t i = 0; i < strlen(json); i++) {
@@ -73,21 +101,32 @@ void lexer() {
       printf("COMMA ");
       break;
     case 't':
-      token = boolean_token(json, &i);
-      printf("BOOLEAN(%s) ", token);
-      free(token);
-      break;
     case 'f':
       token = boolean_token(json, &i);
       printf("BOOLEAN(%s) ", token);
       free(token);
       break;
     case 'n':
+      token = null_token(json, &i);
+      if (strcmp(token, "null") == 0) {
+        printf("NULL ");
+      }
+      free(token);
       break;
     case '\0':
       printf("EOF ");
       break;
-    default:
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '-':
       token = number_token(json, &i);
       printf("NUMBER(%s) ", token);
       free(token);
